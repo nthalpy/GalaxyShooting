d0782c2075fa062e9bd8213baa8477565051096a
moyamong <daniel123800@gmail.com>
2020-05-16 15:14:03 +0900

    Update RenderTestObject.cs
    test a qube

--- GalaxyShooting/Logic/RenderTestObject.cs
+++ GalaxyShooting/Logic/RenderTestObject.cs
@@ -13,10 +13,57 @@ namespace GalaxyShooting.Logic
      
         public override void Render(WireFrameRenderer renderer)
         {
+            /*
             renderer.EnqueueTriangle(new Triangle(
                 new Vector3(-0.5, -0.5, 0),
                 new Vector3(0.5, 0, 0),
                 new Vector3(0, 0.5, 0)
+            ));
+            */
+
+            //test for a qube
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, 1, 2),
+                new Vector3(1, -1, 2),
+                new Vector3(1, 1, 4)
+            ));
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, 1, 4),
+                new Vector3(1, -1, 4),
+                new Vector3(1, -1, 2)
+            ));
+
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(-1, 1, 2),
+                new Vector3(-1, -1, 2),
+                new Vector3(-1, 1, 4)
+            ));
+
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(-1, 1, 4),
+                new Vector3(-1, -1, 4),
+                new Vector3(-1, -1, 2)
+            ));
+
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, 1, 2),
+                new Vector3(-1, 1, 2),
+                new Vector3(1, 1, 4)
+            ));
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, 1, 4),
+                new Vector3(-1, 1, 4),
+                new Vector3(-1, 1, 2)
+            ));
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, -1, 2),
+                new Vector3(-1, -1, 2),
+                new Vector3(1, -1, 4)
+            ));
+            renderer.EnqueueTriangle(new Triangle(
+                new Vector3(1, -1, 4),
+                new Vector3(-1, -1, 4),
+                new Vector3(-1, -1, 2)
             ));
         }
     }
