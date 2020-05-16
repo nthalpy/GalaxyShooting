2b02567f81bd15af3ba4bb77be5509bb058a0c6f
Harnel <dcrgkev@gmail.com>
2020-05-16 20:55:47 +0900

    Quaternion 곱하는 부분 로직 수정

--- GalaxyShooting/Logic/RenderTestObject.cs
+++ GalaxyShooting/Logic/RenderTestObject.cs
@@ -1,4 +1,5 @@
 ﻿using GalaxyShooting.Input;
+using GalaxyShooting.Model;
 using GalaxyShooting.Rendering;
 using System;
 
@@ -9,68 +10,27 @@ namespace GalaxyShooting.Logic
     /// </summary>
     public sealed class RenderTestObject : GameObjectBase
     {
-        private Vector3[] vbo;
-        private int[] ebo;
-
-        private Triangle[] tris;
-
-        private double theta;
-
-        private double yaw;
-        private double pitch;
-        private double roll;
-
         public Vector3 Position;
         public Quaternion RotationX;
         public Quaternion RotationY;
         public Quaternion RotationZ;
 
+        private Matrix4x4 currentRotationMatrix;
+
         public RenderTestObject()
         {
-            vbo = new Vector3[]
-            {
-                new Vector3(-1, -1, -1),
-                new Vector3(-1, -1, 1),
-                new Vector3(-1, 1, -1),
-                new Vector3(-1, 1, 1),
-                new Vector3(1, -1, -1),
-                new Vector3(1, -1, 1),
-                new Vector3(1, 1, -1),
-                new Vector3(1, 1, 1),
-            };
-            ebo = new int[]
-            {
-                0, 6, 4,
-                0, 2, 6,
-                0, 3, 2,
-                0, 1, 3,
-                2, 7, 6,
-                2, 3, 7,
-                4, 6, 7,
-                4, 7, 5,
-                0, 4, 5,
-                0, 5, 1,
-                1, 5, 7,
-                1, 7, 3
-            };
-
-            tris = new Triangle[ebo.Length / 3];
-            for (int idx = 0; idx < ebo.Length / 3; idx++)
-            {
-                tris[idx] = new Triangle(
-                    vbo[ebo[3 * idx]],
-                    vbo[ebo[3 * idx + 1]],
-                    vbo[ebo[3 * idx + 2]]
-                );
-            }
+            currentRotationMatrix = new Matrix4x4(
+                new Vector4(1, 0, 0, 0),
+                new Vector4(0, 1, 0, 0),
+                new Vector4(0, 0, 1, 0),
+                new Vector4(0, 0, 0, 1));
         }
 
         public override void Update()
         {
-            if (InputManager.IsPressed(VK.LEFT))
-                theta -= Math.PI / 30;
-            if (InputManager.IsPressed(VK.RIGHT))
-                theta += Math.PI / 30;
+            double yaw = 0;
+            double pitch = 0;
+            double roll = 0;
 
             if (InputManager.IsPressed(VK.KEY_S))
                 pitch += Math.PI / 30;
@@ -85,10 +45,14 @@ namespace GalaxyShooting.Logic
             if (InputManager.IsPressed(VK.KEY_E))
                 roll += Math.PI / 30;
 
-            Position = new Vector3(5 * Math.Cos(theta), 5 * Math.Sin(theta), 7);
-            RotationX = Quaternion.AxisAngle(new Vector3(1, 0, 0), roll);
-            RotationY = Quaternion.AxisAngle(new Vector3(0, 1, 0), pitch);
-            RotationZ = Quaternion.AxisAngle(new Vector3(0, 0, 1), yaw);
+            Position = new Vector3(0, 0, 7);
+
+            Quaternion rotX = Quaternion.AxisAngle(new Vector3(1, 0, 0), roll);
+            Quaternion rotY = Quaternion.AxisAngle(new Vector3(0, 1, 0), pitch);
+            Quaternion rotZ = Quaternion.AxisAngle(new Vector3(0, 0, 1), yaw);
+
+            Matrix4x4 rotMatrixChange = Matrix4x4.CreateRotationMatrix(rotX) * Matrix4x4.CreateRotationMatrix(rotY) * Matrix4x4.CreateRotationMatrix(rotZ);
+            currentRotationMatrix *= rotMatrixChange;
         }
 
         public override void Render(WireFrameRenderer renderer)
@@ -96,9 +60,9 @@ namespace GalaxyShooting.Logic
             // TODO: implement rotation @moyamong
             Matrix4x4 modelMatrix = Matrix4x4.CreateTranslateMatrix(Position);
 
-            Matrix4x4 rotateMatrix = Matrix4x4.CreateRotationMatrix(RotationZ) * (Matrix4x4.CreateRotationMatrix(RotationY) * Matrix4x4.CreateRotationMatrix(RotationX));
+            Matrix4x4 rotateMatrix = currentRotationMatrix;
 
-            foreach (Triangle triangle in tris)
+            foreach (Triangle triangle in TestPlane.Tris)
             {
                 renderer.EnqueueTriangle(new Triangle(
                     (modelMatrix * (rotateMatrix * triangle.A.ToXYZ1())).HomogeneousToXYZ(),
