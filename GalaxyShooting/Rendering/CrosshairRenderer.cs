71741b016fd3f7082307cae96ee87e38253f21cf
moyamong <daniel123800@gmail.com>
2020-05-17 17:02:50 +0900

    crosshair 모양 변경

--- GalaxyShooting/Rendering/CrosshairRenderer.cs
+++ GalaxyShooting/Rendering/CrosshairRenderer.cs
@@ -11,15 +11,26 @@
         {
             const int sizeX = 20;
             const int sizeY = 20;
-            const int width = 3;
+            const int width = 2;
 
-            for (int dy = 0; dy < width; dy++)
-                for (int dx = 0; dx < sizeX; dx++)
+            int dx = 0;
+            int dy = 0;
+
+            for (dy = 0; dy < width; dy++)
+                for (dx = 0; dx < sizeX; dx++)
+                {
+                    if (dx > sizeX / 4 && dx < sizeX * 3 / 4)
+                        continue;
                     Screen.SetPixel(screenSizeX / 2 - sizeX / 2 + dx, screenSizeY / 2 - width / 2 + dy, true);
+                }
 
-            for (int dx = 0; dx < width; dx++)
-                for (int dy = 0; dy < sizeY; dy++)
+            for (dx = 0; dx < width; dx++)
+                for (dy = 0; dy < sizeY; dy++)
+                {
+                    if (dy > sizeY / 4 && dy < sizeY * 3 / 4)
+                        continue;
                     Screen.SetPixel(screenSizeX / 2 - width / 2 + dx, screenSizeY / 2 - sizeY / 2 + dy, true);
+                }
         }
     }
 }
